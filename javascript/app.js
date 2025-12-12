fced866935a84c3f93f7fcaf643d5069031249ee
Darling <DuarteDarling@outlook.com>
2025-12-12 15:07:31 -0400

    se agrega documento html

--- javascript/app.js
+++
@@ -1,9 +0,0 @@
-const acordeon = document.getElementsByClassName('content');  // retorla los valores
-for (i=0; i<acordeon.length; i++)
-  {   
-    acordeon[i].addEventListener('click' function()
-    {
-      this.classlist.toggle('activa')
-    })
-  }
-
